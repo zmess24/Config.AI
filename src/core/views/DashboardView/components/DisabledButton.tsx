ebfdc0026a10e28c4222ef8ca91c77020e160aed
Zac Messinger <zdmessinger@gmail.com>
2024-05-04 12:17:21 -0700

    deleted

--- src/core/views/DashboardView/components/DisabledButton.tsx
+++
@@ -1,9 +0,0 @@
-function DisabledButton() {
-	return (
-		<button disabled={true} className="dark:bg-gray-600 bg-gray-300 text-gray-500 dark:text-white border-gray-300 dark:border-gray-500 focus:ring-gray-500">
-			Enable Provider
-		</button>
-	)
-}
-
-export default DisabledButton
